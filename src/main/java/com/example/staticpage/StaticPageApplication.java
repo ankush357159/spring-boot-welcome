c122e256cf92c4ff764cecd84cc949e64da68b90
ankush357159 <ankush357159@gmail.com>
2022-08-31 10:31:26 +0530

    First commit

---
+++ src/main/java/com/example/staticpage/StaticPageApplication.java
@@ -0,0 +1,13 @@
+package com.example.staticpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaticPageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StaticPageApplication.class, args);
+    }
+
+}
